1aa2d847b13de547230fbc9ef929f98830b2d1be
Vũ Hoàng Khang <160208535+khangvhdevs@users.noreply.github.com>
2025-08-08 19:52:16 +0700

    S7-104.Refactoring the File Storage Code

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controller/error');
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,9 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000)
--- routes/shop.js
+++ routes/shop.js
@@ -1,21 +1,9 @@
-const path = require('path');
-
 const express = require('express');
-const adminData = require('./admin');
+
+const productsController = require('../controller/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
-    res.render('shop', {
-        pageTitle:'Shop', 
-        prods: products, 
-        docTitle: 'Shop', path:'/', 
-        hasProducts: products.length > 0, 
-        activeShop: true,
-        productCSS: true
-    });
-})
+router.get('/', productsController.getProducts);
 
 module.exports = router;
